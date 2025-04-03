c520eca95d92070c51d7dabc67082344c570c66b
minglecm07 <minglecm@gmx.com>
2025-04-02 21:06:33 -0400

    Added basic stuff to buy and sell and tried to start working on the market
    Problems: initial_value for Stock wasn't an atribute so it wouldnt print the value, but the change in value might work

--- MockStock.py
+++ MockStock.py
@@ -1,8 +1,17 @@
+import time
+import random
 class Stock:
     def __init__(self):
         self.name = None
         self.initial_value = random.randint(5, 100)
 
+    def update_value():
+        Stock.initial_value += 1
+
+        while True:
+            Stock.update_value()
+            time.sleep(10)
+            
 class Player:
     def __init__(self, name):
         self.username = name
@@ -10,13 +19,19 @@ class Player:
         self.numstock = 0
 
     def sell():
+        Player.balance + Stock.initial_value
+        Player.numstock - 1
         return
 
     def buy():
+        Player.balance - Stock.initial_value
+        Player.numstock + 1
         return
 
     def view_market():
+        print(Stock.initial_value)
         return
 
     while True:
         break
+print(Stock.initial_value)
--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import time
 from MockStock import *
 
 player = Player("Patrick")
-
+print(Stock.initial_value)
 print(player.username)
-
-
+while True:
+    playerinput = input("Type view market to view the market value")
+    if playerinput.lower() == "view market":
+        Player.view_market()
+    else:
+        print("that wasnt right );")
 
