93b31d0c9906ee1e278e4b2d7be1f0f74c88e307
p-mcinerney <patrick.o.mcinerney@gmail.com>
2025-04-04 22:04:06 -0400

    Used PyInstaller + New Features
    - .exe file for all users
    - users can choose to look at high scores or play the game

--- app.py
+++ app.py
@@ -1,41 +1,62 @@
 from MockStock import *
 
-username = input("Username: ")
+# TODO - Add an input where the user can choose whether to play the game or look at high scores set
+# TODO - Get an input where the user either types in "p" to play or "h" for highscores
 
-# initialize the objects
-player = Player(username)
-stock = Stock()
+    # TODO - If they ask to play, then run the program as normal and play the game
+    # TODO - If they ask for highscores, load all the highscores from the score.txt file, and print the top 5 highest scores
+while True:
+    play = input("Press 'p' to play or press 'h' to look at the highscores: ")
+    
+    if play.lower() == "p":
+        username = input("Username: ")
 
-# do a for loop for only 10 turns the user has, after 10 rounds the game ends
-for i in range(10):
-    print("")
-    # print simple information
-    if i == 9:
-        print("FINAL ROUND")
-    else:
-        print(f"Round {i + 1}")
-    print(f"Value of {stock.name}: ${stock.current_value}")
-    print(f"Bank: ${player.balance} | Amount of stock: {player.numstock}")
-    while True:
-        msg = input("Action: ")
-        if msg.lower() == "sell":
-            if player.numstock > 0:
-                player.numstock, player.balance = player.sell(stock.current_value, player.numstock, player.balance)
-                break
+        # initialize the objects
+        player = Player(username)
+        stock = Stock()
+
+        # do a for loop for only 10 turns the user has, after 10 rounds the game ends
+        for i in range(10):
+            print("")
+            # print simple information
+            if i == 9:
+                print("FINAL ROUND")
             else:
-                print("You have no stock to sell.")
-                continue
-        elif msg.lower() == "buy":
-            player.numstock, player.balance = player.buy(stock.current_value, player.numstock, player.balance)
-            break
-        elif msg.lower() == "skip":
-            break
-        else:
-            print("That wasnt right")
-    # update the stock value after the round is over
-    stock.update_value()
+                print(f"Round {i + 1}")
+            print(f"Value of {stock.name}: ${stock.current_value}")
+            print(f"Bank: ${player.balance} | Amount of stock: {player.numstock}")
+            
+            while True:
+                msg = input("Action: ")
+                if msg.lower() == "sell":
+                    if player.numstock > 0:
+                        player.numstock, player.balance = player.sell(stock.current_value, player.numstock, player.balance)
+                        break
+                    else:
+                        print("You have no stock to sell.")
+                        continue
+                elif msg.lower() == "buy":
+                    player.numstock, player.balance = player.buy(stock.current_value, player.numstock, player.balance)
+                    break
+                elif msg.lower() == "skip":
+                    break
+                else:
+                        print("That wasnt right.\nThe actions you may perform are 'buy', 'sell', or 'skip'.")
+                # update the stock value after the round is over
+            stock.update_value()
 
-print(f"Final Score\n{player.username} | ${player.balance} | {stock.name}")
+        print(f"Final Score\n{player.username} | ${player.balance} | {stock.name}")
 
-with open("score.txt", "a") as file:
-    file.write(f"Username: {player.username} | Score: {player.balance}\n")
+        with open("score.txt", "a") as file:
+            file.write(f"{player.username}/{player.balance}\n")
+        
+        break
+    
+    elif play.lower() == "h":
+        with open("score.txt", "r") as file:
+            for line in file:
+                name, score = line.split("/")
+                print(f"Username: {name} | Score: {score}", end="")
+        break
+    else:   
+        print("Error: Use 'p' or 'h'")
